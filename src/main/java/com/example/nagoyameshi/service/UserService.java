6659051bd835a05ed01411d94def5e2a09307dcb
Yukohana <jpsky@outlook.jp>
2025-06-11 14:16:09 +0900

    feature-admin-users

--- src/main/java/com/example/nagoyameshi/service/UserService.java
+++ src/main/java/com/example/nagoyameshi/service/UserService.java
@@ -2,7 +2,10 @@ package com.example.nagoyameshi.service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,21 @@ import com.example.nagoyameshi.repository.UserRepository;
 
 @Service
 public class UserService {
+
+	public Page<User> findAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    // 指定されたキーワードを氏名またはフリガナに含むユーザーを、ページングされた状態で取得する
+    public Page<User> findUsersByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageable) {
+        return userRepository.findByNameLikeOrFuriganaLike("%" + nameKeyword + "%", "%" + furiganaKeyword + "%", pageable);
+    }
+
+    // 指定したidを持つユーザーを取得する
+    public Optional<User> findUserById(Integer id) {
+        return userRepository.findById(id);
+    }	
+	
    private final UserRepository userRepository;
    private final RoleRepository roleRepository;
    private final PasswordEncoder passwordEncoder;
@@ -23,6 +41,8 @@ public class UserService {
        this.userRepository = userRepository;
        this.roleRepository = roleRepository;
        this.passwordEncoder = passwordEncoder;
+       
+          
    }
 
    @Transactional
